098b4461444dbd50d2011e7c76e0aa626733399c
wsrcoder <wellsousadev@gmail.com>
2023-03-09 15:36:41 -0300

    Login: Implementacao da parte de cadastro para a tela de login 'Neon'

--- Login/Neon/script.js
+++ Login/Neon/script.js
@@ -8,3 +8,7 @@ const wrapper = document.querySelector('.wrapper')
 signUpBtnLink.addEventListener('click', ()=> {
     wrapper.classList.toggle('active')
 })
+
+signInBtnLink.addEventListener('click', () =>{
+    wrapper.classList.toggle('active')
+})
