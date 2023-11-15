7e63e3560f3deb45cd8bdae8ce18ed5cc9b3ae36
Wellington de Sousa Reis <wsr.coder@gmail.com>
2023-11-15 10:32:45 -0300

    atualização no modelo de projetos do repositorio

---
+++ Login/002/js/login.js
@@ -0,0 +1,16 @@
+
+
+function on_login()
+{
+    window.location.replace("tela_principal_responsavel.html");
+}
+
+function on_criar_conta_responsavel()
+{
+
+}
+
+function on_criar_conta_orientador()
+{
+    
+}
---
+++ Login/login.md
@@ -0,0 +1,9 @@
+
+
+### Descrição
+
+![tela de Login](login_001.jpg)
+
+### Descrição
+
+![tela de Login](login_002.jpg)
